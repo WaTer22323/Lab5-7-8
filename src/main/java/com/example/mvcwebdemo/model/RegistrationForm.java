6551456d4be2bab8c8d6d9bd95dc9d1cf904c957
NN <Pongsapat>
2026-02-12 02:24:16 +0000

    merge branch peem

--- src/main/java/com/example/mvcwebdemo/model/RegistrationForm.java
+++ src/main/java/com/example/mvcwebdemo/model/RegistrationForm.java
@@ -15,6 +15,8 @@ public class RegistrationForm {
     @NotBlank(message = "Country is required")
     private String country;
 
+
+    
     @Past(message = "Date of Birth must be in the past")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dob;
